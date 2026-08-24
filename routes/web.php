d886a65c73603312582291408e73aa727682bfc1
Nama Kamu <nasywa_wijaya_30rpl@student.smktelkom-mlg.sch.id>
2026-08-24 10:45:45 +0700

    Redirect root route to localhost:3000

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json([
-        'name' => 'Nortis AI Assessment API',
-        'framework' => 'Laravel 11',
-        'php_version' => PHP_VERSION,
-        'status' => 'online'
-    ]);
+    return redirect('http://localhost:3000');
 });
